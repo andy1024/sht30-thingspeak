b32d00e5436e2c3c45db7b9cc0030d3fc3392d40
andy1024 <Andrzej.Maslowski1024@gmail.com>
2020-09-26 17:15:29 +0200

    added SGP30 air quality sensor

--- storage-watcher.ino
+++ storage-watcher.ino
@@ -3,16 +3,29 @@
 #include <WEMOS_SHT3X.h>
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
+#include <DS18B20.h>
+#include "Adafruit_SGP30.h"
 
 #include "config.h"
 
 const char* REST_API_END_POINT = "https://api.thingspeak.com";
 
-const int TIMEOUT = 3600000;
-const long DEEP_SLEEP_TIMEOUT = 1.8e9;
+const int TIMEOUT = 600000;//10min
+//1hr//3600000;
+//const long DEEP_SLEEP_TIMEOUT = 1.8e9; //30min
+const long DEEP_SLEEP_TIMEOUT = 6.0e8; //10min
+
+/* sensor equipment */
+/* temperature probe */
+DS18B20 ds(D4);
+uint8_t address[] = {40, 250, 31, 218, 4, 0, 0, 52};
+uint8_t selected;
 
 /* temperature and humidity sensor */
 SHT3X sht30(0x45);
+
+/* air quality sensor */
+Adafruit_SGP30 sgp;
 
 WiFiClient client;
 
@@ -27,7 +40,15 @@ void setup(void)
     delay(500);
     Serial.print(".");
   }
+  Serial.print("DS Devices: ");
+  Serial.println(ds.getNumberOfDevices());
+  Serial.println();
   Serial.println("");
+  if (! sgp.begin()){
+    Serial.println("SGP Sensor not found :(");
+    delay(500);
+    while (1);
+  }
   Serial.println("WiFi connected");
   ThingSpeak.begin(client); 
   Serial.println("ThingSpeak client initialized");
@@ -35,29 +56,69 @@ void setup(void)
 
 void loop(void) {
   Serial.println("<<loop iteration>>");
+  float temperature = -1000;
+  float humidity = -1000;
+  String sAirTemp = "";
+  String sAirHum = "";
+  String sTVOC = "";
+  String seCO2 = "";
+  String sFluidTemp = "";
+  
   if(sht30.get()==0) {
       Serial.println("sht30 reading ok");
-      String sTemp = String(sht30.cTemp);
-      String sHum = String(sht30.humidity);
-      Serial.print("Temperature [C]: ");
-      Serial.println(sTemp);
-      Serial.print("Relative Humidity [%]: ");
-      Serial.println(sHum);
-      ThingSpeak.setField(1, sTemp);
-      ThingSpeak.setField(2, sHum);
+      temperature = sht30.cTemp;
+      humidity = sht30.humidity;
+      sAirTemp = String(temperature);
+      sAirHum = String(humidity);
+      Serial.print("Air Temperature [C]: ");
+      Serial.println(sAirTemp);
+      Serial.print("Air Relative Humidity [%]: ");
+      Serial.println(sAirHum);
 
-      int HTTP_RETURN_CODE = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-      // Check the return code
-      if(HTTP_RETURN_CODE == 200){
-        Serial.println("Channel update successful.");
-      }
-      else{
-        Serial.println("Problem updating channel. HTTP error code " + String(HTTP_RETURN_CODE));
-      }
+      Serial.print("Fluid Temperature [C]: ");
+      sFluidTemp = String(ds.getTempC());
+      Serial.println(sFluidTemp);
   } else {
     Serial.println("SHT30 not found!");
   }
+  if (temperature >-1000 && humidity >-1000) {
+    sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
+  }
+  if (! sgp.IAQmeasure()) {
+    Serial.println("SGP30 Measurement failed");
+  } else {
+    sTVOC = sgp.TVOC;
+    seCO2 = sgp.eCO2;
+    Serial.print("TVOC "); 
+    Serial.print(sTVOC); 
+    Serial.print(" ppb\t");
+    Serial.print("eCO2 "); 
+    Serial.print(seCO2); 
+    Serial.println(" ppm");
+  }
   Serial.println("<<~iteration>>");
-  ESP.deepSleep(DEEP_SLEEP_TIMEOUT);//twice-an hour total
+  ThingSpeak.setField(1, sAirTemp);
+  ThingSpeak.setField(2, sAirHum);
+  ThingSpeak.setField(4, sFluidTemp);
+  ThingSpeak.setField(5, sTVOC);
+  ThingSpeak.setField(6, seCO2);
+
+  int HTTP_RETURN_CODE = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  // Check the return code
+  if(HTTP_RETURN_CODE == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(HTTP_RETURN_CODE));
+  }
+
+  //ESP.deepSleep(DEEP_SLEEP_TIMEOUT);//twice-an hour total
   ESP.deepSleep(DEEP_SLEEP_TIMEOUT);
+}
+
+uint32_t getAbsoluteHumidity(float temperature, float humidity) {
+    // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
+    const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
+    const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
+    return absoluteHumidityScaled;
 }
